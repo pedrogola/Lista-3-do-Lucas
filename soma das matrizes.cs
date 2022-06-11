be6091f06fde95f7df3514e5a1562af2978197b5
pedrogola <107320440+pedrogola@users.noreply.github.com>
2022-06-11 15:06:51 -0300

    Add files via upload

---
+++ soma das matrizes.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SomaDasMatrizes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            
+            int calma;
+            Console.WriteLine("Olá! digite numeros para ver a soma das matrizes");
+            calma = Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
+
+            int[,] A = new int[4, 4];
+            int[,] B = new int[4, 4];
+            
+
+
+
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    Console.WriteLine("insira numeros");
+                    
+                    
+                    A[i, j] = Convert.ToInt32(Console.ReadLine());
+
+                    Console.Clear();
+
+                }
+            }
+
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    Console.WriteLine("insira numeros");
+                    B[i, j] = Convert.ToInt32(Console.ReadLine());
+                    Console.Clear();
+                }
+            }
+
+            int[,] C = soma(A, B);
+
+
+            View(C);
+            ;
+            Console.ReadKey();
+        }
+        public static int[,] soma(int[,] A, int[,] B)
+        {
+            int[,] C = new int[4, 4];
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    C[i, j] = A[i, j] + B[i, j];
+                }
+            }
+           
+
+            return C;
+        }
+        public static void View(int[,] a)
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                    Console.Write("" + "\t" + a[i, j]);
+                Console.WriteLine();
+            }
+
+        }
+        
+
+    }
+}
